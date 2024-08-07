35a3196e90a0af6dd0fca72c9be2d4475d639f92
diegoperea20 <diegoperea20@gmail.com>
2024-08-07 10:24:51 -0500

    added link project

--- src/app/page.js
+++ src/app/page.js
@@ -49,7 +49,7 @@ function Page() {
       <InputFile theme="clear" /> {/* theme clear */}
       <div className="project-github">
         <p>This project is in </p>
-        <Link href="https://github.com/diegoperea20">
+        <Link href="https://github.com/diegoperea20/React-File-Input-Component">
           <img width="96" height="96" src="https://img.icons8.com/fluency/96/github.png" alt="github"/>
         </Link>
       </div>
